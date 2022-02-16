f84345e17c272e06c96c81d662c6ae43c837a2ac
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-02-16 20:34:06 +0000

    9th commit upload for 'Fetch-APIs.com-Responsive'
    1. Added the navigation popup for the 'movieApi.html', with 'movie_api_script.js'.

--- movie_api_script.js
+++ movie_api_script.js
@@ -52,3 +52,45 @@ const random_movie_btn = document.getElementById('random_movie_btn').addEventLis
         }
     }); 
 });
+
+//Hamburger Menu
+
+//Grabbing the hamburger image
+const hamburger_image = document.getElementById('hamburger_image');
+
+//Grabbing the popup section 
+const popup = document.getElementById('popup');
+
+//Grabbing the unordered list within the popup menu
+const ham_ul = document.getElementById('ham_ul');
+
+const close_popup = document.getElementById('close_popup');
+
+//Function that'll enable the popup menu
+function enableMenu(){
+    popup.classList = [];
+    popup.classList.add("popup");
+    // ham_ul.style.margin = "0px 100px";
+    ham_ul.style.transform = "translate(0px,0px)";
+    ham_ul.style.left = "0";
+}
+
+function disablemenu(){
+    popup.classList = [];
+    popup.classList.add("disabled");
+    ham_ul.style.margin = "0px -600px";
+}
+
+//When the user presses the hamburger menu
+hamburger_image.addEventListener("click",(e)=>{
+
+    let x = popup.classList[0];
+ 
+    if(x === "disabled"){
+        enableMenu();
+    }
+});
+
+close_popup.addEventListener("click", (e)=>{
+    disablemenu();
+});
