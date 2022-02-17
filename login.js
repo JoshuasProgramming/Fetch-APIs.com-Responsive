70dcd85f6bc875dd9055f5396f8e5f27696a54d8
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-02-17 11:41:18 +0000

    10th commit upload for 'Fetch-APIs.com-Responsive'
    1. removed the alert from login.js file.

--- login.js
+++ login.js
@@ -4,7 +4,6 @@ const login_password = document.getElementById('login_password');
 
 //Grab login password checkbox and add an event listener for when the user clicks it
 const login_password_checkbox = document.getElementById('login_password_checkbox').addEventListener("click", (e)=>{
-    alert("1");
 
     //Checking if the login password input is of type 'password' or 'text'
     if(login_password.type == "password"){
