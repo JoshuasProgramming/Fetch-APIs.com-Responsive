69ebcad03076f774ee305744f91866ecab6ce904
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-02-17 11:26:48 +0000

    9th commit upload for 'Fetch-APIs.com-Responsive'
    1. Made a login page 
    2. Login page is fully responsive.

---
+++ login.js
@@ -0,0 +1,15 @@
+
+//Grab login password element
+const login_password = document.getElementById('login_password');
+
+//Grab login password checkbox and add an event listener for when the user clicks it
+const login_password_checkbox = document.getElementById('login_password_checkbox').addEventListener("click", (e)=>{
+    alert("1");
+
+    //Checking if the login password input is of type 'password' or 'text'
+    if(login_password.type == "password"){
+        login_password.type = "text";
+    } else if(login_password.type == "text"){
+        login_password.type ="password";
+    }
+});
